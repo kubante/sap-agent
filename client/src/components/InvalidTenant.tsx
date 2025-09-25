5945689673fc75dba4b88fe6ec6fa3cb2f78240e
kubante <kubante@proton.me>
2025-09-25 18:07:52 +0300

    Add links to valid tenants on InvalidTenant Component

--- client/src/components/InvalidTenant.tsx
+++ client/src/components/InvalidTenant.tsx
@@ -1,14 +1,33 @@
-import { Paper, Typography } from "@mui/material";
+import { Paper, Typography, Button, Box } from "@mui/material";
+import { Link } from "react-router-dom";
+import { TENANTS } from "../constants";
+import { capitalize } from "lodash";
 
 export default function InvalidTenant() {
   return (
-    <Paper sx={{ p: 3, minHeight: "calc(100vh - 200px)" }}>
+    <Paper sx={{ p: 3 }}>
       <Typography variant="h4" gutterBottom>
         Invalid Tenant
       </Typography>
-      <Typography variant="body1">
-        Please navigate to a valid tenant page.
+      <Typography variant="body1" sx={{ mb: 3 }}>
+        Please select a valid tenant to continue:
       </Typography>
+      <Box sx={{ display: "flex", gap: 2, flexWrap: "wrap" }}>
+        {TENANTS.map((tenant) => (
+          <Button
+            key={tenant}
+            variant="contained"
+            component={Link}
+            to={`/${tenant}`}
+            sx={{
+              minWidth: 120,
+              textTransform: "capitalize",
+            }}
+          >
+            {capitalize(tenant)}
+          </Button>
+        ))}
+      </Box>
     </Paper>
   );
 }
