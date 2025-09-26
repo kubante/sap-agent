af2e1127383c24d4a0b3e62aeb7a293ec0fa541b
kubante <kubante@proton.me>
2025-09-26 14:26:33 +0300

    Fix vitest.config for React testing

--- client/src/pages/request/__tests__/CountryFetcher.test.tsx
+++ client/src/pages/request/__tests__/CountryFetcher.test.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { render } from "@testing-library/react";
 import { screen, waitFor } from "@testing-library/dom";
 import userEvent from "@testing-library/user-event";
